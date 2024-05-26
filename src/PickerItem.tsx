3bc51ae6e701c6764ecba0e6ad10c914441f6e67
Javier Diaz <coderdiaz@gmail.com>
2024-05-25 23:11:59 -0600

    fix: fixed style prop on picker item

--- src/PickerItem.tsx
+++ src/PickerItem.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { ColorValue, StyleProp, Text, TextStyle } from 'react-native';
+import { StyleProp, Text, TextStyle } from 'react-native';
 
 export interface PickerItemProps {
-  color?: ColorValue;
   label: string;
   value: string | number;
   testID?: string;
   style?: StyleProp<TextStyle>;
 }
 
-export const PickerItem = ({ color, label, ...props }: PickerItemProps) => {
-  return (
-    <Text style={{ color }} {...props}>
-      {label}
-    </Text>
-  );
+export const PickerItem = ({ label, style, ...props }: PickerItemProps) => {
+  return <Text {...props}>{label}</Text>;
 };
